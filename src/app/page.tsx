e75f8a29facdf99b76381ef0b8818703c78346e2
Thae Myat Noe Htut <u6520209@au.edu>
2026-05-24 23:04:58 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,7 @@ function Lobby() {
               onClick={() => createRoom()}
               className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50"
             >
-              CREATE SECURE ROOM
+              CREATE A SECURE ROOM
             </button>
           </div>
         </div>
